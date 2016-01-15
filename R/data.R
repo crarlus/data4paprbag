d8bcf611e1c701e603eaca8c743d6ac241967b73
unknown <denekec@NU-NB-06640.rki.local>
2016-01-15 23:16:23 +0100

    manual created

--- R/data.R
+++ R/data.R
@@ -1,4 +1,13 @@
 #' small classifier for training fold 1
-#' 
-#' 
+#'
+#'
 "classifier_fold1_small"
+
+#' Pathogenicity labels
+#' @details
+#' The set of labels for 4286 bacterial strains (181 non-pathogens and 2838 pathogens)
+#' TRUE means pathogenic and FALSE means non-pathogenic
+#' The species' bioproject accession is obtained by names(Labels)
+#' @references
+#' PaPrBag Paper
+"Labels"
