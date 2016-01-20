7c7e397306adab218a6d9208cead812f0e14b8e8
crarlus <denekec@rki.de>
2016-01-20 17:25:13 +0100

    data description added

--- R/data.R
+++ R/data.R
@@ -11,3 +11,40 @@
 #' @references
 #' PaPrBag Paper
 "Labels"
+
+
+#' Random forest classifier based on small training data set, fold1
+#' @details The small dataset is composed of 200,000 sequencing reads
+"classifier_small_fold1.rda"
+
+#' Random forest classifier based on small training data set, fold2
+#' @details The small dataset is composed of 200,000 sequencing reads
+"classifier_small_fold2.rda"
+
+#' Random forest classifier based on small training data set, fold3
+#' @details The small dataset is composed of 200,000 sequencing reads
+"classifier_small_fold3.rda"
+
+#' Random forest classifier based on small training data set, fold4
+#' @details The small dataset is composed of 200,000 sequencing reads
+"classifier_small_fold4.rda"
+
+#' Random forest classifier based on small training data set, fold5
+#' @details The small dataset is composed of 200,000 sequencing reads
+"classifier_small_fold5.rda"
+
+
+#' Bioproject accessions and their labels for bacterial strains used in fold 1
+"labels_fold1.rda"
+
+#' Bioproject accessions and their labels for bacterial strains used in fold 2
+"labels_fold2.rda"
+
+#' Bioproject accessions and their labels for bacterial strains used in fold 3
+"labels_fold3.rda"
+
+#' Bioproject accessions and their labels for bacterial strains used in fold 4
+"labels_fold4.rda"
+
+#' Bioproject accessions and their labels for bacterial strains used in fold 5
+"labels_fold5.rda"
